bb6fd0609e3371396cefe132595d76b459716ed3
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-05-11 14:10:45 +0200

    almost done with the seacrh feature and most of menu

--- src/Menu.java
+++ src/Menu.java
@@ -25,12 +25,28 @@ public class Menu {
 
         switch (firstChoice){
             case 0:
-                //login with a profile - check with E-mail and Password and make sure they login to the correct acccount
-                //user = database.login();**************''
+                user = database.login();
+                menu();
                  break;
             case 1:
-                //create a user - make sure that the person is 18 or above otherwise the
-                // program will not let you create and account.
+                System.out.println("Please enter E-Mail");
+                String email = textUI.get();
+                System.out.println("Please enter Password");
+                String password = textUI.get();
+                System.out.println("Please enter first name:");
+                String firstName = textUI.get();
+                System.out.println("Please enter last name:");
+                String lastName = textUI.get();
+                System.out.println("Please enter age:");
+                int age = textUI.getInteger();
+
+                if(age < 18){
+                    System.out.println("Apologies, but you are not old enough to register on this application");
+                }
+                else {
+                    user = new Person(email, password, firstName, lastName, age);
+                    menu();
+                }
                 break;
             case 2:
                 guestMenu();
@@ -38,13 +54,11 @@ public class Menu {
         }
     }
 
-
-
     private void menu() {
         boolean on = true;
 
         while(on){
-            String[] options = {"Search", "User details","Surprise me", "Quit"};
+            String[] options = {"Search", "User details", "Surprise me", "Quit"};
             int optionChoice = textUI.select("Choose an option", options, "");
 
             switch(optionChoice){
@@ -98,16 +112,170 @@ public class Menu {
                     System.out.println("Thanks for using Drunk Drunk go");
                     on = false;
                     break;
-
-
             }
-
-
         }
 
     }
 
     public void search(){
+        boolean check = true;
+        boolean alcoholChoiceCheck = true;
+        String nameChoice = null;
+        String typeChoice = null;
+        int priceChoice = 0;
+        String notesChoice = null;
+        String countrychoice = null;
+        String like = "Like";
+
+        boolean beerChoice = false;
+        boolean wineChoice = false;
+        boolean spiritChoice = false;
+
+
+        String[] alcoholChoices = {"beer", "wine", "spirit","Continue to search criteria"};
+        int alcoholToSearchFor = textUI.select("Please select which alcohol you would like to search for", alcoholChoices, "");
+        while(alcoholChoiceCheck){
+            switch (alcoholToSearchFor){
+                case 0:
+                    System.out.println("You have selected to search for beer");
+                    beerChoice = true;
+                    break;
+                case 1:
+                    System.out.println("You have selected to search for wine");
+                    wineChoice = true;
+                    break;
+                case 2:
+                    System.out.println("You have selected to search for spirits");
+                    spiritChoice = true;
+                    break;
+                case 3:
+                    alcoholChoiceCheck = false;
+                    break;
+            }
+        }
+        try{
+            connection = DriverManager.getConnection(JdbcUrl, username, password);
+
+
+            while(check) {
+                String[] searchChoices = {"Name","Type","Price", "Notes", "Country","Complete Search"};
+                int searchChoice = textUI.select("Please select search criteria(s)", searchChoices, "");
+                switch (searchChoice) {
+
+                    case 0:
+                        System.out.println("Please enter the name of the alcohol you would like to search for");
+                        nameChoice = textUI.get();
+                        break;
+                    case 1:
+                        System.out.println("Please enter the type of alcohol you would like to search for e.g. white wine or pilsner");
+                        typeChoice = textUI.get();
+                        break;
+                    case 2:
+                        System.out.println("please enter a max amount of money you would like to spend");
+                        priceChoice = textUI.getInteger();
+                        break;
+                    case 3:
+                        System.out.println("Please enter notes you would like to search for e.g. oak, berries or sweet");
+                        notesChoice = textUI.get();
+                        break;
+                    case 4:
+                        System.out.println("Please enter which country you would like to search for");
+                        countrychoice = textUI.get();
+                        break;
+                    case 5:
+                        if(nameChoice == null){
+                            nameChoice = "IS NOT NULL";
+                            like = "";
+                        }
+                        if(typeChoice == null){
+                            typeChoice = "IS NOT NULL";
+                        }
+                        if(notesChoice == null){
+                            notesChoice = "IS NOT NULL";
+                        }
+                        if(countrychoice == null){
+                            countrychoice = "IS NOT NULL";
+                        }
+
+
+
+                        PreparedStatement statement1 = connection.prepareStatement("SELECT * FROM iceprojekt.beer WHERE Name ? '%?%' && " +
+                                "Type = '?' && Price <= ? && Notes ? '%?%' && Country ? '%?%'; ");
+                        if(beerChoice) {
+                            statement1.setString(1, like);
+                            statement1.setString(2, nameChoice);
+                            statement1.setString(3, typeChoice);
+                            statement1.setInt(4, priceChoice);
+                            statement1.setString(5, like);
+                            statement1.setString(6, notesChoice);
+                            statement1.setString(7, like);
+                            statement1.setString(8, countrychoice);
+                        }
+
+                        PreparedStatement statement2 = connection.prepareStatement("SELECT * FROM iceprojekt.wine WHERE Name ? '%?%' && " +
+                                "Type = '?' && Price <= ? && Notes ? '%?%' && Country ? '%?%'; ");
+                        if(wineChoice) {
+                            statement2.setString(1, like);
+                            statement2.setString(2, nameChoice);
+                            statement2.setString(3, typeChoice);
+                            statement2.setInt(4, priceChoice);
+                            statement2.setString(5, like);
+                            statement2.setString(6, notesChoice);
+                            statement2.setString(7, like);
+                            statement2.setString(8, countrychoice);
+                        }
+
+                        PreparedStatement statement3 = connection.prepareStatement("SELECT * FROM iceprojekt.spirits WHERE Name ? '%?%' && " +
+                                "Type = '?' && Price <= ? && Notes ? '%?%' && Country ? '%?%'; ");
+                        if(spiritChoice) {
+                            statement3.setString(1, like);
+                            statement3.setString(2, nameChoice);
+                            statement3.setString(3, typeChoice);
+                            statement3.setInt(4, priceChoice);
+                            statement3.setString(5, like);
+                            statement3.setString(6, notesChoice);
+                            statement3.setString(7, like);
+                            statement3.setString(8, countrychoice);
+                        }
+
+                        ResultSet resultSet1 = statement1.executeQuery();
+                        ResultSet resultSet2 = statement1.executeQuery();
+                        ResultSet resultSet3 = statement1.executeQuery();
+
+
+                        while (resultSet1.next()) {
+
+                            String beerName = resultSet1.getString("Name");
+                            String beerType = resultSet1.getString("numOfPlayers");
+                            String beerPrice = resultSet1.getString("points");
+                            String goalDiff = resultSet1.getString("goalDiff");
+
+                            String teamBuild = teamName + ", " + numOfPlayers + ", " + points + ", " + goalDiff;
+
+
+                            teamData[counter] = teamBuild;
+
+                            counter++;
+                        }
+
+
+                        break;
+
+
+
+                }
+
+            }
+
+
+
+        }
+        catch(SQLException e){
+           e.printStackTrace();
+
+        }
+
+
 
     }
 
